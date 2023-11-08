941d3519f80a611501271c19c901330b58375878
Kira Solovay <kirasolovay@gmail.com>
2023-11-08 09:54:44 -0500

    Get Even answered

--- software_homework/answers.js
+++ software_homework/answers.js
@@ -2,3 +2,9 @@
 for (let i = 1; i <=20; i++){
     console.log(i);
 }
+
+// write a for loop that wil log only the even numbers 0 through 200
+for (let i = 0; i < 200; i += 2){
+    console.log(i)
+}
+
